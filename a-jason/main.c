ce5d651fad2c2600bee915cd73224dafeb816f85
JasonZhi <369610501@qq.com>
2019-09-21 02:30:06 +0800

    build my dir

--- a-jason/main.c
+++ a-jason/main.c
@@ -1,7 +1,36 @@
 #include <stdio.h>
+#include <lib/common.h>
+
+void read_data(int sockfd) {
+    ssize_t n;
+    char buf[1024];
+
+    int time = 0;
+    for (;;) {
+        fprintf(stdout, "block in read\n");
+        if ((n = readn(sockfd, buf, 1024)) == 0)
+            return;
+
+        time++;
+        fprintf(stdout, "1K read for %d \n", time);
+        usleep(1000);
+    }
+}
+
 
 int main() {
-    printf("hello world");
+
+    int listenfd, connfd;
+    socklen_t clilen;
+    struct sockaddr_in cliaddr, servaddr;
+
+    listenfd = socket(AF_INET, SOCK_STREAM, 0);
+    bzero(&servaddr, sizeof(servaddr));
+    servaddr.sin_family = AF_INET;
+    servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
+    servaddr.sin_port = htons(12345);
+
+    bind(listenfd, (struct sockaddr) &servaddr)
 
     return 0;
 }
